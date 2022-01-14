b50bdc1ccb83788ce0e56c970f8e64e9e0b7cb7f
Aalaap Ghag <aalaap@gmail.com>
2022-01-14 13:11:46 +0530

    Add app name and authors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
+    const NAME: &str = env!("CARGO_PKG_NAME");
     const VERSION: &str = env!("CARGO_PKG_VERSION");
+    const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
 
-    println!("Doe v{}", VERSION);
+    println!("{} v{}", NAME, VERSION);
+    println!("Copyright (c) 2021 {}", AUTHORS);
+
 }
